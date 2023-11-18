4f5d2b129fedff801270a570cb108eeb1ae846ba
sahill67 <150814702+sahill67@users.noreply.github.com>
2023-11-19 03:48:45 +0530

    Update errors.sol

--- errors.sol
+++ errors.sol
@@ -1,34 +1,21 @@
-// SPDX-License-Identifier: MIT
+ // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ErrorHandlingExample {
-    function evenSquare(uint256 number) public pure returns (uint256) {
-        // Using require to check if the number is even
-        require(number % 2 == 0, "Input number must be even");
-        
-        // Calculate and return the square
-        return number * number;
-    }
+contract NumberGuessingGame {
+    uint public winningNumber = 21;
+    address public winner;
 
-    function oddCube(uint256 number) public pure returns (uint256) {
-        // Using revert to trigger an error if the number is not positive and odd
-        if (!(number > 0 && number % 2 != 0)) {
-            revert("Input number must be positive and odd");
-        }
-        
-        // Calculate and return the cube
-        return number * number * number;
-    }
+    event Winner(address winner);
 
-    function calculateFactorial(uint256 n) public pure returns (uint256) {
-        // Using assert to ensure the input is positive
-        assert(n > 0);
-        
-        // Calculate and return the factorial
-        uint256 result = 1;
-        for (uint256 i = 1; i <= n; i++) {
-            result *= i;
+    function guessNumber(uint chosenNumber) public {
+        assert(chosenNumber > 17);
+        require(chosenNumber >= 18 && chosenNumber <= 35, "Number must be between 18 and 35");
+
+        if (chosenNumber == winningNumber) {
+            winner = msg.sender;
+            emit Winner(winner);
+        } else {
+            revert("Wrong number chosen. Try again!");
         }
-        return result;
     }
 }
